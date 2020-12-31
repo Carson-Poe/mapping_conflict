d331a10c586736710cb6a86e4ab98cedb0153365
Carson Poe <carpoe@gmail.com>
2020-12-31 13:49:01 -0800

    Project near completion. ('near' 'completion', lol)

--- code/practice_data.R
+++ code/practice_data.R
@@ -16,6 +16,7 @@ library(gganimate)
 library(plotly)
 library(ggplot2)
 library(sf)
+library(lubridate)
 
 # Retrieving and Cleaning Data -------------------------------------------
 
@@ -130,6 +131,10 @@ load("C:/Users/Car/Desktop/Messing Around/Exploration Mapping/mapping_conflict/d
 
 # Fast Exploration --------------------------------------------------------
 
+# tibble
+c_df <- c_df %>% 
+    as_tibble(c_df)
+
 # USA involved
 View(c_df %>%
     filter(side_b_new_id == 769 | side_a_new_id == 769))
@@ -140,35 +145,33 @@ View(c_df)
 # Wow, this function is nice
 skim(c_df)
 
+# Create a civilian deaths categorical variable
+c_df <- c_df %>% 
+    mutate(civ_cat = case_when(deaths_civilians > 0 ~ 'yes', TRUE ~ 'no'))
+
+# How many?
+c_df %>% 
+    count(civ_cat)
+
+# This would make a great variable to predict. 
+
 # This is looking better already. Starting to see some interesting stuff. Ish
 # Way better since I've been watching Julia Silge and David Robinson
-# Get count by country
+
+# Okay, loving this plot, group by country and rounded dates, sum deaths, plot
+# Filtering by more than 300 events here.
 c_df %>% 
-    select(country, best, date_start, region, best) %>% 
     add_count(country) %>% 
-    filter(n > 100) %>% 
-    ggplot(aes(date_start, best, color = country)) +
+    filter(n > 300) %>% 
+    mutate(rounded_date = floor_date(as.Date(date_start), unit = 'month')) %>% 
+    group_by(country, rounded_date) %>% 
+    mutate(sum_deaths = sum(best)) %>% 
+    select(country, sum_deaths, rounded_date) %>% 
+    ggplot(aes(rounded_date, sum_deaths, color = country)) +
     geom_line() +
+    theme(axis.text.x = element_text(angle = 45, vjust = .5), legend.position = 'none') +
+    labs(x = 'Dates', y = 'Total Deaths') +
     facet_wrap(~country, scales = 'free_y')
-
-# The problem with the above graph is it does up and down lines, like, the 
-# 'best estimate' isn't a great variable to plot here, we could do boxplots
-
-c_df %>% 
-    select(country, best, date_start, region, best) %>% 
-    add_count(country) %>% 
-    filter(n > 100, best < 300) %>% 
-    ggplot(aes(y = best, x = country)) +
-    geom_boxplot() +
-    labs(y = 'Deaths', x = 'Country') +
-    facet_wrap(~country, scales = 'free')
-
-# That looks a bit better, also, it seems like plotting by best isn't a great idea
-# maybe average by country, first attempt didn't work, I couldn't pass the mutate
-# into the geom_ properally, I had multi rows per country. I wonder, gggplot might
-# not allow for grouped to be passed in. 
-# Solution was to create a new df, though I probably could have done all the summarize stuff
-# down there. Actually, lets try that
 
 # Okay, really cool plot. Doesn't show st dev, but shows average deaths, plus sample
 # size, ordered by most
@@ -179,6 +182,7 @@ c_df %>%
     group_by(country) %>% 
     mutate(average = (sum(best)/n), sum_deaths = sum(best)) %>% 
     select(-best, -date_start) %>% 
+    # Seems like a cheater way to limit number of rows
     summarise(region = unique(region),
           n = max(n), 
           average = max(average),
@@ -189,13 +193,15 @@ c_df %>%
     geom_text(aes(label = paste0('n= ', n), angle = 90), hjust = 'top') +
     theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position =  'none') +
     labs(x = 'Country', y = 'Average Deaths per Violent Encounter 2019') +
-    scale_y_continuous( n.breaks = 8)
+    scale_y_continuous(n.breaks = 8)
 
 
 # This ggpairs fucntion is great, can really throw stuff in and see what sticks
 c_df %>%
-    select(type_of_violence, region, best) %>%
-    filter(best < 100) %>%
+    add_count(region) %>% 
+    filter(best < 100) %>% 
+    filter(n > 100) %>% 
+    select(civ_cat, region, best) %>%
     ggpairs(mapping = aes(color = region))
 
 # Need type of to be factor
@@ -203,60 +209,51 @@ c_df$type_of_violence <- as.factor(c_df$type_of_violence)
 
 # This is beautiful. Easy ggplot for histogram of deaths, w/ Region Facet
 c_df %>%
-    filter(best <50) %>%
+    filter(best < 50) %>%
     ggplot(aes(x = best)) +
     geom_histogram(bins = 50, aes(fill=region)) + 
     facet_wrap(~region) +
-    xlab('Deaths Best Estimate')
-
-# Practice Plots
-ggplot(c_df, aes(group = type_of_violence, y=best, x = date_end)) +
-    geom_line()
-
-ggplot(c_df, aes(group = type_of_violence, y=best, x = type_of_violence, fill = type_of_violence)) +
-    geom_bar(stat = 'identity') +
-    scale_color_viridis(discrete = TRUE, option = "D") +
-    scale_fill_viridis(discrete = TRUE)
-
-ggplot(c_df, aes(group = type_of_violence, y=best, x = type_of_violence, fill = type_of_violence)) +
-    geom_boxplot(stat = 'identity') +
-    scale_color_viridis(discrete = TRUE, option = "D") +
-    scale_fill_viridis(discrete = TRUE)
+    xlab('Deaths Best Estimate') +
+    labs(title = 'Are some regions events more deadly?', y ='Number of Events')
 
 # Mapping Conflict Data ---------------------------------------------------
 
 # One is state based, two is non-state based, three is one sided
 # OMG THIS IS IT. THE WORLD MAP! Thank you JULIA SILGE! 
 world <- map_data('world')
-View(c_df)
+
+# 
 ggplot() +
     geom_map(data = world, map = world, aes(long, lat, map_id = region), 
              color = 'white', fill = 'gray50', alpha = .2) +
     geom_point(data = c_df, aes(longitude, latitude, color = as.factor(type_of_violence)), 
                alpha = .8) +
     theme(legend.title = element_text('Type of Violence')) +
-    labs(title = 'Global Violence')
-    scale_color_brewer(palette = 'Set1')
-
-# Size is deaths, world map
-world <- map_data('world')
-View(c_df)
+    labs(title = 'Global Violence') +
+    scale_color_brewer(palette = 'Set1', labels = c('State Based', 'Non-State', 'One-Sided')) +
+    guides(color = guide_legend('Type of Violence'))
 
 
+type_of_violence
+
+
+#Type of UCDP organized violence:
+    #1: state-based conflict
+    #2: non-state conflict
+    #3: one-sided violence
+
+# Okay, kinda like, civilian deaths mapped globally
 ggplot() +
     geom_map(data = world, map = world, aes(long, lat, map_id = region), 
-             color = 'white', fill = 'gray50', alpha = .2) +
-    geom_point(data = c_df, aes(longitude, latitude, color = as.factor(type_of_violence),
-                                size = best, group = id), 
-               alpha = .8) +
+             color = 'white', fill = 'gray50', alpha = .3) +
+    geom_point(data = c_df, aes(longitude, latitude, 
+                                color = as.factor(civ_cat), 
+                                group = id), alpha = .8) +
     theme() +
-    scale_color_brewer(palette = 'Set1')
+    scale_color_brewer(palette = 'Dark2') +
+    labs(title = 'Conflict Civilian Deaths 2019', x ='', y= '') +
+    guides(color = guide_legend('Civilian Deaths'))
 
-
-
-# tibble
-c_df <- c_df %>% 
-    as_tibble()
 
 # January only, change to dates
 c_jan_19 <- c_df %>% 
@@ -334,12 +331,11 @@ afg <-
          title = 'Conflict in Afghanistan January 2019')
 
 # Plotly instead?
-afg <- ggplotly(afg)
+afg <- ggplotly(afg, width = 500, height = 500)
 
 # Need to re-run to see if this fixes sizing - 
 afg %>% 
-    animation_opts(1000, easing = "linear", redraw = FALSE) %>% 
-    layout(autosize = F, width = 500, height = 500, margin = m)
+    animation_opts(1000, easing = "linear", redraw = FALSE) 
 
 
 
@@ -348,3 +344,53 @@ c_jan_19_af
 
 # save it and we shall see
 htmlwidgets::saveWidget(afg, 'afg.html')
+
+
+#  India -------------------------------------------------------------------
+
+# Set up data frame
+# January only, India only, change to dates, select specific columns
+c_jan_19_in <- c_df %>% 
+    filter(country == 'India') %>%
+    select(id, best, latitude, longitude, side_a, side_b, date_start, date_end) %>% 
+    mutate(date_start = as.Date(date_start), date_end = as.Date(date_end)) %>% 
+    filter(date_start <= '2019-1-31' & date_start >= '2019-1-1') 
+
+# Get shapefile -- https://hub.arcgis.com/datasets/2b63527870ef416bacf83bcaf388685f_0/data
+india_sf <- read_sf('data/india')
+
+# Beautiful, needed ids and frame for plotly. Frame needs to be in numeric or prob character
+# maybe as.Date as.char
+ind <- 
+    ggplot(data = c_jan_19_in) +
+    geom_sf(data = india_sf, fill = 'gray50', alpha =.1) +
+    geom_point(data = c_jan_19_in, aes(longitude, latitude, ids = id,
+                                       frame = as.character(date_start)),
+               alpha = .8, color = 'darkred', size = 1) +
+    labs(x = 'Longitude', 
+         y = 'Latitude',
+         title = 'Conflict in India January 2019')
+
+# Plotly instead?
+ind <- ggplotly(ind, width = 500, height = 500)
+
+# Add opts
+ind %>% 
+    animation_opts(1000, easing = "linear", redraw = FALSE) 
+
+
+
+# save it and we shall see
+htmlwidgets::saveWidget(ind, 'india.html')
+
+
+# Modeling ----------------------------------------------------------------
+
+# Can we predict whether there were civilian deaths? 
+
+# Use strata
+
+# How many?
+c_df %>% 
+    count(civ_cat)
+
